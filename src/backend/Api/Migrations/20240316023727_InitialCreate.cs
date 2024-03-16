6f0ac1b27d011c9911232812c3e4ce213890b649
Tom <15997448+tomarks@users.noreply.github.com>
2024-03-16 12:37:57 +1000

    Add initial migration

---
+++ src/backend/Api/Migrations/20240316023727_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DirectoryNodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    ParentDirectoryId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DirectoryNodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DirectoryNodes_DirectoryNodes_ParentDirectoryId",
+                        column: x => x.ParentDirectoryId,
+                        principalTable: "DirectoryNodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Extension = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    Data = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    ParentDirectoryId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Documents_DirectoryNodes_ParentDirectoryId",
+                        column: x => x.ParentDirectoryId,
+                        principalTable: "DirectoryNodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DirectoryNodes_ParentDirectoryId",
+                table: "DirectoryNodes",
+                column: "ParentDirectoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_ParentDirectoryId",
+                table: "Documents",
+                column: "ParentDirectoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropTable(
+                name: "DirectoryNodes");
+        }
+    }
+}
