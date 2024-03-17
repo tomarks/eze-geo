3b4e934f00d61140e466818a688755c7ab5f29e8
Tom <15997448+tomarks@users.noreply.github.com>
2024-03-17 13:13:37 +1000

    App Shell mostly done

---
+++ src/frontend/src/components/breadcrumbs/directory-breadcrumbs.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export const DirectoryBreadcrumbs = (props: Props) => {
+  return (
+	<div>DirectoryBreadcrumbs</div>
+  )
+}
--- src/frontend/src/features/home/home-page.tsx
+++ src/frontend/src/features/home/home-page.tsx
@@ -1,39 +1,70 @@
 import { useEffect, useState } from 'react';
 import { Client, DirectoryStructure } from '../../../generated/client';
-import { DirectoryPicker } from '../../components/directory-picker/directory-picker-component';
+import { DirectoryPicker } from '../../components/directory-picker/directory-picker.component';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  DialogTitle,
+  Paper,
+  TextField,
+} from '@mui/material';
+import { DirectoryBreadcrumbs } from '../../components/breadcrumbs/directory-breadcrumbs.component';
 
 export const Home = () => {
-
-  const [directoryStructure, setDirectoryStructure] = useState<DirectoryStructure|null>(null)
+  const [directoryStructure, setDirectoryStructure] =
+    useState<DirectoryStructure | null>(null);
 
   const api = import.meta.env.VITE_API;
 
-
   useEffect(() => {
-    const api = new Client(import.meta.env.VITE_API)
+    const api = new Client(import.meta.env.VITE_API);
 
-    api.documentDirectoriesGET().then(res => {
-      setDirectoryStructure(res)
+    api.documentDirectoriesGET().then((res) => {
+      setDirectoryStructure(res);
     });
-  
-    return () => {
 
-    }
-  }, [])
-  
-
-
-  
+    return () => {};
+  }, []);
 
   return (
     <>
-      <div className="flex justify-center align-middle ">
-        <div className="flex w-fit flex-col gap-2"></div>
+      <Paper
+        className="m-5 min-h-[calc(100vh-80px)] w-[calc(100vw-80px)] p-5 flex flex-col"
+        elevation={5}
+      >
+        <div className="m-5 flex flex-row gap-5">
+          <div className="flex flex-row gap-5 ">
+            <Button variant="contained">Create Folder</Button>
+            <TextField
+              id="folderNameInput"
+              label="Folder Name"
+              variant="standard"
+            />
+          </div>
+          <div className='grow flex justify-end flex-row'>
+          <Button variant="contained">Upload File</Button>
 
+          </div>
 
-        <DirectoryPicker></DirectoryPicker>
+        </div>
+        <div className="m-5 flex flex-1 h-full flex-row gap-5">
+            <Card elevation={2} className=" h-full w-96">
+              <CardHeader className="text-left" title="Folders" />
+              <CardContent>
+                <DirectoryPicker />
+              </CardContent>
+            </Card>
 
-      </div>
+            <Card elevation={2}  className='w-full'>
+              <CardHeader className="text-left" title={<DirectoryBreadcrumbs />} />
+              <CardContent className="">
+                <div>Content...</div>
+              </CardContent>
+            </Card>
+        </div>
+      </Paper>
     </>
   );
 };
